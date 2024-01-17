e74fb42e8ef333d379477d7f5cf25dc117d1d41a
S-kkipie <admamaniq@unsa.edu.pe>
2024-01-17 14:38:24 -0500

    funciona el sistema de login usando fetch para recibir los datos del usuario

--- cgi-bin/login.pl
+++ cgi-bin/login.pl
@@ -2,3 +2,47 @@
 use strict;
 use warnings;
 use CGI;
+use strict;
+use warnings;
+use CGI;
+use CGI::Cookie;
+use DBI;
+use JSON;
+use Digest::SHA qw(sha1_hex);
+my $db_name   = 'trabajofinal';
+my $db_user   = 'root';
+my $db_pass   = '';
+my $db_host   = 'localhost';
+my $table     = 'users';
+sub generar_token_sesion {
+    my $tiempo_actual = time();
+    my $token = sha1_hex($tiempo_actual, rand());
+    return $token;
+}
+my $cgi = CGI->new;
+my $password = $cgi->param('password') // '';
+my $email  = $cgi->param('email') // '';
+my $dbh = DBI->connect("DBI:mysql:database=$db_name;host=$db_host", $db_user, $db_pass)
+  or die "No se pudo conectar a la base de datos: $DBI::errstr";
+
+my $query = "SELECT * FROM $table WHERE email = ? AND password = ?";
+my $sth = $dbh->prepare($query);
+$sth->execute($email, $password) or die "Error en la ejecución de la consulta: $DBI::errstr";
+
+if (my $usuario = $sth->fetchrow_hashref) {
+    my $token_sesion = generar_token_sesion();
+    my $submitToken = "UPDATE $table SET token_sesion = ? WHERE email = ?";
+    my $stm = $dbh->prepare($submitToken);
+    $stm->execute($token_sesion, $email) or die "Error en la consulta";
+
+    my $cookie = CGI::Cookie->new(
+        -name    => 'token_sesion',
+        -value   => $token_sesion,
+        -expires => '+1d',
+    );
+    print $cgi->header(-cookie => $cookie, -type => 'application/json');
+    print '{"success": true}';
+} else {
+    print $cgi->header(-type => 'application/json');
+    print '{"success": false}';
+}
--- cgi-bin/register.pl
+++ cgi-bin/register.pl
@@ -5,29 +5,34 @@ use CGI;
 use CGI::Cookie;
 use DBI;
 use JSON;
+use Digest::SHA qw(sha1_hex);
 my $db_name   = 'trabajofinal';
 my $db_user   = 'root';
 my $db_pass   = '';
 my $db_host   = 'localhost';
 my $table     = 'users';
+sub generar_token_sesion {
+    my $tiempo_actual = time();
+    my $token = sha1_hex($tiempo_actual, rand());  # Usando sha1_hex de Digest::SHA
+
+    return $token;
+}
+my $token_sesion = generar_token_sesion();
 my $cgi = CGI->new;
 my $name = $cgi->param('name');
 my $password = $cgi->param('password');
 my $email  = $cgi->param('email');
 my $telefono  = $cgi->param('telefono');
 my $cui  = $cgi->param('cui');
-my $token_sesion = "a5c3787d2eef7c4bcf3d37b5b1512a5"; # Debes ajustar esto
 my $dbh = DBI->connect("DBI:mysql:database=$db_name;host=$db_host", $db_user, $db_pass)
   or die "No se pudo conectar a la base de datos: $DBI::errstr";
 my $sql = "INSERT INTO $table (name, email, password, telefono, cui, token_sesion) VALUES (?, ?, ?, ?, ?, ?)";
 my $sth = $dbh->prepare($sql);
 $sth->execute($name, $email, $password, $telefono, $cui, $token_sesion);
 $dbh->disconnect;
-# Configura una cookie con el token de sesión
 my $cookie = CGI::Cookie->new(
     -name    => 'token_sesion',
     -value   => $token_sesion,
-    -expires => '+1d', # Puedes ajustar la vigencia de la cookie según tus necesidades
+    -expires => '+1d',
 );
-# Envía la cookie al cliente
 print $cgi->header(-cookie => $cookie, -type => 'text/html', -status => '302 Found', -location => '../index.html');
