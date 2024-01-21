a66dc7964b31fb7c4aa30cefed1e29e78aa0bbe1
S-kkipie <admamaniq@unsa.edu.pe>
2024-01-21 16:42:31 -0500

    xd

--- js/miPerfil.js
+++ js/miPerfil.js
@@ -49,3 +49,43 @@ changeSettingBilling.addEventListener("click", () => {
         }
     }, 500);
 })
+let editButtons = document.querySelectorAll(".editInformationButton");
+let informationContainer = document.querySelectorAll(".informationContainer");
+let p = document.querySelectorAll(".informationContainer p");
+let inputs = [];
+let saveButtons = [];
+function buttonsEventListener() {
+    for (let i = 0; i < editButtons.length; i++) {
+        inputs.push(document.createElement("input"));
+        saveButtons.push(document.createElement("button"));
+        saveButtons[i].classList.add("editInformationButton");
+        saveButtons[i].innerHTML = "Guardar";
+        inputs[i].name = editButtons[i].id.substring(4).toLowerCase();
+        console.log(inputs[i].name);
+        editButtons[i].addEventListener("click", () => {
+            editButtons[i].style.display = "none";
+            informationContainer[i].append(inputs[i], saveButtons[i]);
+            inputs[i].value = datos[inputs[i].name];
+            p[i].style.display = "none";
+        });
+        saveButtons[i].addEventListener("click", () => {
+            const form = new FormData();
+            form.append("columna", inputs[i].name);
+            form.append("valor", inputs[i].value);
+            form.append("token_sesion", obtenerTokenDeSesion());
+            console.log(form);
+            fetch("cgi-bin/editInformation.pl", {
+                method: 'POST',
+                body: form
+            }).then(response => response.json())
+                .then(data => {
+                    if (data.success != true) {
+                        alert(data.success)
+                    } else {
+                        alert("Datos Actualizados Correctamente");
+                    }
+                    window.location.reload();
+                });
+        });
+    }
+}
