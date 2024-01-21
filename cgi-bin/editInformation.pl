7058dcecd266d6e0625cc460c51e8bcacf016e7d
S-kkipie <admamaniq@unsa.edu.pe>
2024-01-21 00:46:07 -0500

    correccion del nombre de la url de fetcg denuevo correcion del nombre de una variable

--- cgi-bin/editInformation.pl
+++ cgi-bin/editInformation.pl
@@ -10,29 +10,28 @@ my $cgi = CGI->new;
 my $token_sesion = $cgi->param('token');
 my $new_data = $cgi->param('newdata');
 my $decoded_json = decode_json($new_data);
-my $clave = keys %$decoded_json;
-my $valor = values %$decoded_json;
-if ($token_sesion, $clave, $valor) {
-        my $datos_usuario = cargar_datos_a_tabla($token_sesion);
-        if ($datos_usuario) {
-            print $cgi->header(-type => 'application/json', -status => '200 OK');
-            print '{"success": true}';
-        } else {
-            print $cgi->header(-type => 'text/plain', -status => '500 Internal Server Error');
-            print '{"success": false}';
-
-        }
-}else{
+my @claves = keys %$decoded_json;
+my @valores = values %$decoded_json;
+if ($token_sesion && @claves && @valores) {
+    my $datos_usuario = cargar_datos_a_tabla($token_sesion, \@claves, \@valores);
+    if ($datos_usuario) {
+        print $cgi->header(-type => 'application/json', -status => '200 OK');
+        print '{"success": true}';
+    } else {
+        print $cgi->header(-type => 'text/plain', -status => '500 Internal Server Error');
+        print '{"success": false}';
+    }
+} else {
     print $cgi->header(-type => 'text/plain', -status => '500 Internal Server Error');
     print '{"success": false}';
-
 }
+
 sub cargar_datos_a_tabla {
-    my ($token, $columna, $valor) = @_;
+    my ($token, $claves, $valores) = @_;
     my $dbh = DBI->connect("DBI:mysql:database=trabajofinal;host=localhost", "root", "753159", { RaiseError => 1 });
-    my $query = "UPDATE users SET $columna = ? WHERE token_sesion = ?";
+    my $query = "UPDATE users SET $claves[0] = ? WHERE token_sesion = ?";
     my $sth = $dbh->prepare($query);
-    my $filas_afectadas = $sth->execute($valor, $token);
+    my $filas_afectadas = $sth->execute($valores[0], $token);
     $dbh->disconnect();
     return $filas_afectadas;
 }
