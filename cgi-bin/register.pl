635e6b09e79bfdf9a3018c840b08994f110a6b42
Johann-1498 <jcaceresruiz@unsa.edu.pe>
2024-01-23 11:12:06 -0500

    error al registrar usuarios

--- cgi-bin/register.pl
+++ cgi-bin/register.pl
@@ -21,10 +21,9 @@ my $cgi = CGI->new;
 my $name = $cgi->param('name');
 my $password = $cgi->param('password');
 my $email  = $cgi->param('email');
-my $telefono  = $cgi->param('telefono');
+my $telefono  = $cgi->param('phone');
 my $cui  = $cgi->param('cui');
-my $dbh = DBI->connect("DBI:mysql:database=$db_name;host=$db_host", $db_user, $db_pass)
-  or die "No se pudo conectar a la base de datos: $DBI::errstr";
+my $dbh = DBI->connect("DBI:mysql:database=$db_name;host=$db_host", $db_user, $db_pass);
 my $sql = "INSERT INTO $table (name, email, password, telefono, cui, token_sesion) VALUES (?, ?, ?, ?, ?, ?)";
 my $sth = $dbh->prepare($sql);
 $sth->execute($name, $email, $password, $telefono, $cui, $token_sesion);
