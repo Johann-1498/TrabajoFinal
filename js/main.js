1bfd5591f356db1f8bea442def041968720c8b9b
Johann-1498 <jcaceresruiz@unsa.edu.pe>
2024-01-29 17:33:20 -0500

    implementacion de los productos

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ import Item from "./Item.js";
   //Verificando la sesion
   let esValidaLaSesion = false;
   validarSesion()
-    .then((response) => response.json())
+    .then(response => response.json())
     .then((data) => {
       if (data.name) {
         esValidaLaSesion = true;
