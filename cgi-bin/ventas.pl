e0a6b2df48ecffd979dd98843553cdc1cce9da3b
S-kkipie <admamaniq@unsa.edu.pe>
2024-02-08 23:43:28 -0500

    ventas

--- cgi-bin/ventas.pl
+++ cgi-bin/ventas.pl
@@ -2,9 +2,9 @@
 use strict;
 use warnings;
 use CGI;
-use CGI::Cookie;
 use DBI;
 use JSON;
+use Time::Piece;
 my $db_name   = 'trabajofinal';
 my $db_user   = 'root';
 my $db_pass   = '753159';
@@ -20,20 +20,38 @@ my $cgi = CGI->new;
     my $productos = $cgi->param('productos');
     my $clientID  = $cgi->param('clientID');
     my $paymentID = $cgi->param('paymentID');
-    my $fecha     = time();
-
+    my $fecha_actual = localtime;
+    my $fecha = $fecha_actual->strftime("%Y-%m-%d %H:%M:%S");
+    my $json_data = decode_json($productos);
+    my $finalPrice = 0;
+    while (my ($clave, $valor) = each %$json_data) {
+        $valor = decode_json($valor);
+        $finalPrice += $valor->{amount} * $valor->{price};
+    }
     my $dbh = DBI->connect("DBI:mysql:database=$db_name;host=$db_host", $db_user, $db_pass);
-
     my $sql  = "INSERT INTO $table (productos, date, clientID, paymentID, detalles, name, direccion, phone, email) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
     my $sth  = $dbh->prepare($sql);
     my $result = $sth->execute($productos, $fecha, $clientID, $paymentID, $detalles, $name, $direccion, $telefono, $email);
-
     if ($result) {
         my $sql2 = "DELETE FROM carrito WHERE clientID = ?";
         my $sth2  = $dbh->prepare($sql2);
         my $result = $sth2->execute($clientID);
-        print $cgi->header(-type => 'application/json', -status => 200);
-        print '{"success": true}';
+        my $sql3 = "SELECT amount FROM billing_card WHERE cardnumber = ?";
+        my $sth3  = $dbh->prepare($sql2);
+        $sth3->execute($paymentID);
+        my $fila3 =  $sth3->fetchrow_arrayref;
+        my $amount = $fila3->[0];
+        $amount = $amount - $finalPrice;
+        my $sql4 = "UPDATE billing_card SET amount = ? WHERE cardnumber = ?";
+        my $sth4 = $dbh->prepare($sql4);
+        my $rows_affected = $sth->execute($amount, $paymentID);
+        if($rows_affected){
+            print $cgi->header(-type => 'application/json', -status => 200);
+            print '{"success": true}';
+        }else{
+            print $cgi->header(-type => 'application/json', -status => 500);
+            print '{"success": "error al procesar el pago la venta"}';
+        }
     } else {
         print $cgi->header(-type => 'application/json', -status => 500);
         print '{"success": "error al crear la venta"}';
