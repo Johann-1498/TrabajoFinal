b755d785179985bc8574017b498cd9a3e7bac366
S-kkipie <admamaniq@unsa.edu.pe>
2024-02-08 20:58:16 -0500

    busqueda

--- js/scriptIndex.js
+++ js/scriptIndex.js
@@ -204,7 +204,7 @@ function buttonEvent(array) {
   }
 }
 //CODIGO PARA LA BUSQUEDA DE ELEMENTOS
-let inputSearch = document.querySelector("#searchPL");
+let inputSearch = document.querySelector("#searchPl");
 inputSearch.addEventListener("input", () => {
   fetch("cgi-bin/resultadosDeBusqueda.pl?seach=" + inputSearch.value).then(response => response.json()).then(data => data.forEach(value => console.log(value)));
 });
