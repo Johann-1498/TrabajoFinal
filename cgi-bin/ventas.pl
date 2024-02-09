d5bef688e51343f175aef9a1135e146a1ce750c1
S-kkipie <admamaniq@unsa.edu.pe>
2024-02-08 19:51:27 -0500

    implementacion de la independencia del codigo de las categorias

--- cgi-bin/ventas.pl
+++ cgi-bin/ventas.pl
@@ -23,21 +23,39 @@ my $cgi = CGI->new;
     my $fecha     = time();
 
     my $dbh = DBI->connect("DBI:mysql:database=$db_name;host=$db_host", $db_user, $db_pass);
+my $response;
 
-    my $sql  = "INSERT INTO $table (productos, date, clientID, paymentID, detalles, name, direccion, phone, email) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-    my $sth  = $dbh->prepare($sql);
+eval {
+    my $sql   = "INSERT INTO $table (productos, date, clientID, paymentID, detalles, name, direccion, phone, email) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    my $sth   = $dbh->prepare($sql);
     my $result = $sth->execute($productos, $fecha, $clientID, $paymentID, $detalles, $name, $direccion, $telefono, $email);
 
     if ($result) {
-        my $sql2 = "DELETE FROM carrito WHERE clientID = ?";
-        my $sth2  = $dbh->prepare($sql2);
+        my $sql2   = "DELETE FROM carrito WHERE clientID = ?";
+        my $sth2   = $dbh->prepare($sql2);
         my $result = $sth2->execute($clientID);
-        print $cgi->header(-type => 'application/json', -status => 200);
-        print '{"success": true}';
+        $sth2->finish;
+        $response = {
+            success => 1,
+            message => "Venta creada correctamente.",
+        };
     } else {
-        print $cgi->header(-type => 'application/json');
-        print '{"success": "error al crear la venta"}';
+        $response = {
+            success => 0,
+            error   => "Error al crear la venta.",
+        };
     }
     $sth->finish;
-    $dbh->disconnect;
+};
 
+if ($@) {
+    $response = {
+        success => 0,
+        error   => "Error interno en el servidor: $@",
+    };
+}
+
+$dbh->disconnect;
+
+print $cgi->header(-type => 'application/json', -status => 200);
+print '{"success": ' . $response->{success} . ', "message": "' . $response->{message} . '", "error": "' . $response->{error} . '"}';
