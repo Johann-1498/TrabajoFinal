414f46cdf9a352fa3ef9e6abc01cb56e92290bb4
S-kkipie <admamaniq@unsa.edu.pe>
2024-02-08 23:05:44 -0500

    implementando tarjetas

--- js/miPerfil.js
+++ js/miPerfil.js
@@ -41,7 +41,7 @@ function buttonsEventListener() {
         editButtons[i].addEventListener("click", () => {
             editButtons[i].style.display = "none";
             informationContainer[i].append(inputs[i], saveButtons[i]);
-            console.log(editButtons[i].id.substring(0, 4).toLowerCase());
+            console.log(editButtons[i].id.substring(4, 8).toLowerCase());
             if (editButtons[i].id.substring(0, 4).toLowerCase() === "card") {
                 console.log("Xd");
                 inputs[i].value = data[inputs[i].name];
